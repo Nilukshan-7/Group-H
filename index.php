3c12ee7091d3eb458ad07c73460715bb5a7de36c
Dev-MithuShan <mithushan012@gmail.com>
2026-01-02 21:45:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home | Local Solution Network</title>
+  <title>Home | Local Solution Network- LSN</title>
 
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
